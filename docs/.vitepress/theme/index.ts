f4998ee819465535771892111705be98ba2d8ea3
Bjørn Stabell <bjorn@stabell.org>
2026-04-09 16:00:59 -0700

    fix(docs): prevent glossary tooltips from being clipped by table overflow
    VitePress wraps markdown tables in overflow-x:auto containers which
    clips glossary popovers. Override to overflow:visible + z-index for
    tooltip elements.

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,6 +3,7 @@ import Layout from "./Layout.vue"
 import HtmlDiagram from "./components/HtmlDiagram.vue"
 import "vitepress-enrich/css/tooltip.css"
 import "vitepress-enrich/css/glossary-links.css"
+import "./custom.css"
 
 export default {
   extends: DefaultTheme,
